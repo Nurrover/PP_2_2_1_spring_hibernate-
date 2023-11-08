4fa9bfb75da837b1ba4317ea78ca03a3c057694e
Nurrover <nuriksafarov00@gmail.com>
2023-11-08 19:07:45 +0400

    version 1.0

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,45 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+      User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+
+      user1.setCar(new Car("model1", 1));
+      user2.setCar(new Car("model2", 2));
+      user3.setCar(new Car("model3", 3));
+      user4.setCar(new Car("model4", 4));
+
+
+      userService.add(user1);
+      userService.add(user2);
+      userService.add(user3);
+      userService.add(user4);
+
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println(user);
+         System.out.println(user.getCar());
+         System.out.println();
+      }
+
+      System.out.println(userService.getUser("model1", 1));
+
+      context.close();
+   }
+}
